ba36d07d5c13ba8c5c05b8edf4ec97f705c96787
matklad <aleksey.kladov@gmail.com>
2013-06-16 18:07:01 +0400

    more shows for expressions

--- src/Hscool/Types/AST.hs
+++ src/Hscool/Types/AST.hs
@@ -68,7 +68,31 @@ instance Show Expression where
     where
       aux expr = case expr of
         Assign s e -> printf "#1\n_assign\n  %s\n%s" s (indent e)
+        Dispatch e s actuals -> printf "#1\n_dispatch\n%s\n  %s\n  (\n%s\n  )"
+                                (indent e) s (joinAndIndent actuals)
+        StaticDispatch e type_ name actuals ->
+          printf "#1\n_static_dispatch\n%s\n  %s\n  \n%s\n  (\n%s\n  )"
+          (indent e) type_ name (joinAndIndent actuals)
+        Cond e1 e2 e3 -> printf "#1\n_cond\n%s\n%s\n%s"
+                         (indent e1) (indent e2) (indent e3)
+        Loop e1 e2 -> printf "#1\n_loop\n%s\n%s"
+                      (indent e1) (indent e2)
+        Block es -> printf "#1\n_block\n%s" (joinAndIndent es)
+        e1 :+ e2 -> printf "#1\n_plus\n%s\n%s" (indent e1) (indent e2)
+        e1 :- e2 -> printf "#1\n_minus\n%s\n%s" (indent e1) (indent e2)
+        e1 :* e2 -> printf "#1\n_mul\n%s\n%s" (indent e1) (indent e2)
+        e1 :/ e2 -> printf "#1\n_divide\n%s\n%s" (indent e1) (indent e2)
+        Neg e -> printf "#1\n_neg\n%s" (indent e)
+        e1 :< e2 -> printf "#1\n_lt\n%s\n%s" (indent e1) (indent e2)
+        e1 := e2 -> printf "#1\n_eq\n%s\n%s" (indent e1) (indent e2)
+        e1 :<= e2 -> printf "#1\n_leq\n%s\n%s" (indent e1) (indent e2)
+        Comp e -> printf "#1\n_comp\n%s" (indent e)
         IntConst s -> printf "#1\n_int\n  %s" s
+        StringConst s -> printf "#1\n_string\n  %s" s
+        New s -> printf "#1\n_new\n  %s" s
+        IsVoid e -> printf "#1\n_isvoid\n%s" (indent e)
+        Object s -> printf "#1\n_object\n  %s" s
+        _ -> error "can't show this expression"
 
 data Case = Case
           deriving Show
