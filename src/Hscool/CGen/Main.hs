9ec8f7f95bce6173f7f3508a2b5b05b21798b9d0
matklad <aleksey.kladov@gmail.com>
2013-07-02 00:57:06 +0400

    add attribute init rewriting

--- src/Hscool/CGen/Main.hs
+++ src/Hscool/CGen/Main.hs
@@ -3,10 +3,10 @@ import           Control.Applicative     ((<$>))
 import qualified Data.ByteString         as B
 import           Hscool.Types.AST
 import Hscool.CGen.Gen
+import Hscool.CGen.Preprocess
 
 
 main :: IO ()
 main = do
     program <- parseTProgram <$> B.getContents
-    let code = cgen program
-    print code
+    print $ preprocess program
--- src/Hscool/CGen/Preprocess.hs
+++ src/Hscool/CGen/Preprocess.hs
@@ -1,6 +1,28 @@
 module Hscool.CGen.Preprocess where
 
-import           Hscool.Types
+import           Hscool.Types.AST
 
 preprocess :: TProgram -> TProgram
-preprocess = undefined
+preprocess = alphaConvert . makeInits
+
+alphaConvert :: TProgram -> TProgram
+alphaConvert = id
+
+makeInits :: TProgram -> TProgram
+makeInits (Program classes) = let
+        makeInit :: TClass -> TClass
+        makeInit (Class name super features file) = let
+                methods = [m | m@(Method {}) <- features]
+                attributes = [a | a@(Attribute {}) <- features]
+                aux attrs = let (as, ex) = aux $ tail attrs in case attrs of
+                    [] -> ([], [])
+                    a@(Attribute _ _ (Expr "_no_type" NoExpr)):_ -> (a:as, ex)
+                    (Attribute n t e):_ -> ((Attribute n t (Expr "_no_type" NoExpr)):as,
+                                            (Expr t (Assign n e):ex))
+                (newAttrs, ini) = aux attributes
+            in
+                Class name super (newAttrs ++ (Method "_init" [] "Object" (Expr "Object" $ Block ini):methods)) file
+
+    in
+        Program $ map makeInit classes
+
